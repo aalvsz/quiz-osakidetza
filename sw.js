0dd6a68eb409417a6dc96a68decec899514b52c4
Ander Alvarez Sanz <ander.alvarez@ander-alvarez-jdfw43x342.tailb57611.ts.net>
2026-05-04 20:36:00 +0200

    Use root URL as PWA start link

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = "quiz-osakidetza-pwa-2026-05-04-anon-history2";
-const START_URL = "./?v=2026-05-04-anon-history2";
+const CACHE_NAME = "quiz-osakidetza-pwa-2026-05-04-root-link1";
+const START_URL = "./";
 const APP_SHELL = [
   START_URL,
   "./manifest.webmanifest",
